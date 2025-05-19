33b299b6ba9b303eee9d5d457ce2b1d56db6f6bb
Belinda1704 <b.larose@alustudent.com>
2025-05-19 12:43:10 +0200

    Implement image uploads, dynamic pagination, and search functionality

--- pagination_project/urls.py
+++ pagination_project/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
     path('', RedirectView.as_view(pattern_name='books:book_list', permanent=False), name='root_redirect'),
 ] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
